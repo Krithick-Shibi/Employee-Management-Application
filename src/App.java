7d42370f6bc175def8ab1b0339fc99e59b5ab99f
Krithick-Shibi <151458686+Krithick-Shibi@users.noreply.github.com>
2023-11-20 00:22:20 +0530

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n\n\t\t\t\t\tPRESIDIO EMPLOYEE MANAGEMENT APPLICATION");
+        Menu.start();
+    }
+}
